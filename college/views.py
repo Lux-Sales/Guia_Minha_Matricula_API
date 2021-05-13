8a5619c0e32a2933d9abec77493311ea46a86f18
lucas <sales_l@hotmail.com>
2021-05-13 12:35:43 -0300

    Returning  user name

--- college/views.py
+++ college/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import AllowAny
 from college.models import Subject, Teacher, Comment
 from rest_framework import viewsets
 from rest_framework.response import Response
+from users.models import User
 
 class SubjectViewSet(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
@@ -24,15 +25,18 @@ class CommentViewSet(viewsets.ModelViewSet):
         serializer = CommentSerializer(queryset, many=True)
         all_teachers = Teacher.objects.all()
         all_subjects = Subject.objects.all()
+        all_users = User.objects.all()
         all_comments = []
         index = 0
         for comment in queryset.values():
             teacher_id = serializer.data[index]['teacher']
             subject_id = serializer.data[index]['subject']
+            user_id = serializer.data[index]['user']
             teacher = all_teachers.filter(id=teacher_id)
             subject = all_subjects.filter(id=subject_id)
+            user = all_users.filter(id=user_id)
             all_comments.append({'comment':comment['comment'], 'teacher':{'id':teacher_id, 'name':teacher.values()[0]['name']},
-             'subject':{'id':subject_id, 'name':subject.values()[0]['name']}, 'user':comment['user_id'] })
+             'subject':{'id':subject_id, 'name':subject.values()[0]['name']}, 'user':{"user_id":user_id,"name":user.values()[0]['first_name']} })
             index+= 1 
 
         return Response(all_comments)
